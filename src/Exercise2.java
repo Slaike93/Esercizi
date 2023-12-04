1db770d9af68c6ec16b3975828ecc9f312364648
andrea <andrea.av93@gmail.com>
2023-12-04 10:53:48 +0100

    Secondo esercizio: checkNumber

---
+++ src/Exercise2.java
@@ -0,0 +1,8 @@
+public class Exercise2 {
+    int number = 10;
+
+    public int checkNumber(){
+        if (number>10) return 1;
+        else return 2;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,14 @@ public class Main {
         Exercise ex1 = new Exercise();
         ex1.loop();
 
+        System.out.println("---------------------------------------");
+
+        Exercise2 ex2 = new Exercise2();
+        ex2.number =11;
+        System.out.println(ex2.checkNumber());
+
+        System.out.println("---------------------------------------");
+
 
     }
 }
