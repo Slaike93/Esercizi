087ab9c5d5a29a6410bf62d0bbef7bb602f961f6
andrea <andrea.av93@gmail.com>
2023-12-04 10:48:15 +0100

    Primo esercizio

---
+++ src/Exercise.java
@@ -0,0 +1,8 @@
+public class Exercise {
+/*Use the loop() function to create a for loop that goes from 0 to 10 and prints all the numbers to the console.*/
+    public void loop(){
+        for (int i=0; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Exercise ex1 = new Exercise();
+        ex1.loop();
+
+
+    }
+}
